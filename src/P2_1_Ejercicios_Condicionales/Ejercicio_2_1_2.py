89a507dc63d5b6a1708e8a657fdf58a82be4984e
famiguel875 <famiguel@iesrafaelalberti.es>
2023-11-04 23:01:30 +0100

    Push 1.1

--- src/P2_1_Ejercicios_Condicionales/Ejercicio_2_1_2.py
+++ src/P2_1_Ejercicios_Condicionales/Ejercicio_2_1_2.py
@@ -1,16 +1,3 @@
-"""
-password1 = input("Introduzca la contraseña: ")
-password2 = "contraseña"
-
-if password1.upper() == password2.upper():
-    print("Contraseña correcta")
-else:
-    print("Contraseña errónea")
-
-#if __name__ = "__main__":
-    #main()
-"""
-
 def comprobarContraseña(password1, password2):
     if password1.upper() == password2.upper():
         return print("Contraseña correcta")
